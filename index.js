f0e4110be8125affd4c85ef5c9e30568d5afae3e
nihat417 <akreminihat@gmail.com>
2024-05-08 14:26:48 +0400

    Update server create

--- index.js
+++ index.js
@@ -48,3 +48,37 @@
 //------------------------------------
 
 //  Third Homework
+
+const http = require('http');
+const fs = require('fs');
+const { error } = require('console');
+
+//Simple Version
+
+// const server = http.createServer((req,res)=>{
+//     console.log('request made');
+// });
+
+
+//Dif version
+
+const server = http.createServer((req,res)=>{
+    console.log('request made');
+    res.setHeader('Content-Type','text/html');
+    //res.write('Hello Niko');
+    //res.write("<a href='https://www.google.com' target='_blank'>Go to Google </a>");
+    //res.end();
+    fs.readFile("index.html",(err,data)=>{
+        if(err){
+            console.log(`Error : ${err}`);
+            res.end();
+        }
+        res.write(data);
+        res.end();
+    });
+})
+
+
+server.listen(3001,'localhost',()=>{
+    console.log('listning port 3001')
+});
